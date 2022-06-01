c878116c1c35af9bbc4f479e7a3d7b2e388ef669
bayhickimse173@gmail.com <bayhickimse173@gmail.com>
2022-06-01 16:25:12 +0300

    Add project files.

---
+++ Passaparola/Form1.Designer.cs
@@ -0,0 +1,412 @@
+﻿
+namespace Passaparola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.buttonE = new System.Windows.Forms.Button();
+            this.buttonF = new System.Windows.Forms.Button();
+            this.buttonG = new System.Windows.Forms.Button();
+            this.buttonH = new System.Windows.Forms.Button();
+            this.buttonI = new System.Windows.Forms.Button();
+            this.buttonJ = new System.Windows.Forms.Button();
+            this.buttonK = new System.Windows.Forms.Button();
+            this.buttonL = new System.Windows.Forms.Button();
+            this.buttonM = new System.Windows.Forms.Button();
+            this.buttonN = new System.Windows.Forms.Button();
+            this.buttonO = new System.Windows.Forms.Button();
+            this.buttonP = new System.Windows.Forms.Button();
+            this.buttonR = new System.Windows.Forms.Button();
+            this.buttonS = new System.Windows.Forms.Button();
+            this.buttonT = new System.Windows.Forms.Button();
+            this.buttonU = new System.Windows.Forms.Button();
+            this.buttonV = new System.Windows.Forms.Button();
+            this.buttonY = new System.Windows.Forms.Button();
+            this.buttonZ = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCevapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelDogru = new System.Windows.Forms.Label();
+            this.labelYanlis = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonA
+            // 
+            this.buttonA.Location = new System.Drawing.Point(263, 50);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(47, 42);
+            this.buttonA.TabIndex = 0;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = true;
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(316, 50);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(47, 42);
+            this.buttonB.TabIndex = 1;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = true;
+            // 
+            // buttonC
+            // 
+            this.buttonC.Location = new System.Drawing.Point(369, 50);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(47, 42);
+            this.buttonC.TabIndex = 2;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonD
+            // 
+            this.buttonD.Location = new System.Drawing.Point(369, 98);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(47, 42);
+            this.buttonD.TabIndex = 3;
+            this.buttonD.Text = "D";
+            this.buttonD.UseVisualStyleBackColor = true;
+            // 
+            // buttonE
+            // 
+            this.buttonE.Location = new System.Drawing.Point(369, 146);
+            this.buttonE.Name = "buttonE";
+            this.buttonE.Size = new System.Drawing.Size(47, 42);
+            this.buttonE.TabIndex = 4;
+            this.buttonE.Text = "E";
+            this.buttonE.UseVisualStyleBackColor = true;
+            // 
+            // buttonF
+            // 
+            this.buttonF.Location = new System.Drawing.Point(369, 194);
+            this.buttonF.Name = "buttonF";
+            this.buttonF.Size = new System.Drawing.Size(47, 42);
+            this.buttonF.TabIndex = 5;
+            this.buttonF.Text = "F";
+            this.buttonF.UseVisualStyleBackColor = true;
+            // 
+            // buttonG
+            // 
+            this.buttonG.Location = new System.Drawing.Point(369, 242);
+            this.buttonG.Name = "buttonG";
+            this.buttonG.Size = new System.Drawing.Size(47, 42);
+            this.buttonG.TabIndex = 6;
+            this.buttonG.Text = "G";
+            this.buttonG.UseVisualStyleBackColor = true;
+            // 
+            // buttonH
+            // 
+            this.buttonH.Location = new System.Drawing.Point(369, 290);
+            this.buttonH.Name = "buttonH";
+            this.buttonH.Size = new System.Drawing.Size(47, 42);
+            this.buttonH.TabIndex = 7;
+            this.buttonH.Text = "H";
+            this.buttonH.UseVisualStyleBackColor = true;
+            // 
+            // buttonI
+            // 
+            this.buttonI.Location = new System.Drawing.Point(369, 338);
+            this.buttonI.Name = "buttonI";
+            this.buttonI.Size = new System.Drawing.Size(47, 42);
+            this.buttonI.TabIndex = 8;
+            this.buttonI.Text = "I";
+            this.buttonI.UseVisualStyleBackColor = true;
+            // 
+            // buttonJ
+            // 
+            this.buttonJ.Location = new System.Drawing.Point(316, 338);
+            this.buttonJ.Name = "buttonJ";
+            this.buttonJ.Size = new System.Drawing.Size(47, 42);
+            this.buttonJ.TabIndex = 9;
+            this.buttonJ.Text = "J";
+            this.buttonJ.UseVisualStyleBackColor = true;
+            // 
+            // buttonK
+            // 
+            this.buttonK.Location = new System.Drawing.Point(263, 338);
+            this.buttonK.Name = "buttonK";
+            this.buttonK.Size = new System.Drawing.Size(47, 42);
+            this.buttonK.TabIndex = 10;
+            this.buttonK.Text = "K";
+            this.buttonK.UseVisualStyleBackColor = true;
+            // 
+            // buttonL
+            // 
+            this.buttonL.Location = new System.Drawing.Point(210, 338);
+            this.buttonL.Name = "buttonL";
+            this.buttonL.Size = new System.Drawing.Size(47, 42);
+            this.buttonL.TabIndex = 11;
+            this.buttonL.Text = "L";
+            this.buttonL.UseVisualStyleBackColor = true;
+            // 
+            // buttonM
+            // 
+            this.buttonM.Location = new System.Drawing.Point(157, 338);
+            this.buttonM.Name = "buttonM";
+            this.buttonM.Size = new System.Drawing.Size(47, 42);
+            this.buttonM.TabIndex = 12;
+            this.buttonM.Text = "M";
+            this.buttonM.UseVisualStyleBackColor = true;
+            // 
+            // buttonN
+            // 
+            this.buttonN.Location = new System.Drawing.Point(104, 338);
+            this.buttonN.Name = "buttonN";
+            this.buttonN.Size = new System.Drawing.Size(47, 42);
+            this.buttonN.TabIndex = 13;
+            this.buttonN.Text = "N";
+            this.buttonN.UseVisualStyleBackColor = true;
+            // 
+            // buttonO
+            // 
+            this.buttonO.Location = new System.Drawing.Point(51, 338);
+            this.buttonO.Name = "buttonO";
+            this.buttonO.Size = new System.Drawing.Size(47, 42);
+            this.buttonO.TabIndex = 14;
+            this.buttonO.Text = "O";
+            this.buttonO.UseVisualStyleBackColor = true;
+            // 
+            // buttonP
+            // 
+            this.buttonP.Location = new System.Drawing.Point(51, 290);
+            this.buttonP.Name = "buttonP";
+            this.buttonP.Size = new System.Drawing.Size(47, 42);
+            this.buttonP.TabIndex = 15;
+            this.buttonP.Text = "P";
+            this.buttonP.UseVisualStyleBackColor = true;
+            // 
+            // buttonR
+            // 
+            this.buttonR.Location = new System.Drawing.Point(51, 242);
+            this.buttonR.Name = "buttonR";
+            this.buttonR.Size = new System.Drawing.Size(47, 42);
+            this.buttonR.TabIndex = 16;
+            this.buttonR.Text = "R";
+            this.buttonR.UseVisualStyleBackColor = true;
+            // 
+            // buttonS
+            // 
+            this.buttonS.Location = new System.Drawing.Point(51, 194);
+            this.buttonS.Name = "buttonS";
+            this.buttonS.Size = new System.Drawing.Size(47, 42);
+            this.buttonS.TabIndex = 17;
+            this.buttonS.Text = "S";
+            this.buttonS.UseVisualStyleBackColor = true;
+            // 
+            // buttonT
+            // 
+            this.buttonT.Location = new System.Drawing.Point(51, 146);
+            this.buttonT.Name = "buttonT";
+            this.buttonT.Size = new System.Drawing.Size(47, 42);
+            this.buttonT.TabIndex = 18;
+            this.buttonT.Text = "T";
+            this.buttonT.UseVisualStyleBackColor = true;
+            // 
+            // buttonU
+            // 
+            this.buttonU.Location = new System.Drawing.Point(51, 98);
+            this.buttonU.Name = "buttonU";
+            this.buttonU.Size = new System.Drawing.Size(47, 42);
+            this.buttonU.TabIndex = 19;
+            this.buttonU.Text = "U";
+            this.buttonU.UseVisualStyleBackColor = true;
+            // 
+            // buttonV
+            // 
+            this.buttonV.Location = new System.Drawing.Point(51, 50);
+            this.buttonV.Name = "buttonV";
+            this.buttonV.Size = new System.Drawing.Size(47, 42);
+            this.buttonV.TabIndex = 20;
+            this.buttonV.Text = "V";
+            this.buttonV.UseVisualStyleBackColor = true;
+            // 
+            // buttonY
+            // 
+            this.buttonY.Location = new System.Drawing.Point(104, 50);
+            this.buttonY.Name = "buttonY";
+            this.buttonY.Size = new System.Drawing.Size(47, 42);
+            this.buttonY.TabIndex = 21;
+            this.buttonY.Text = "Y";
+            this.buttonY.UseVisualStyleBackColor = true;
+            // 
+            // buttonZ
+            // 
+            this.buttonZ.Location = new System.Drawing.Point(157, 50);
+            this.buttonZ.Name = "buttonZ";
+            this.buttonZ.Size = new System.Drawing.Size(47, 42);
+            this.buttonZ.TabIndex = 22;
+            this.buttonZ.Text = "Z";
+            this.buttonZ.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(104, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(259, 234);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 451);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(252, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonCevapla
+            // 
+            this.buttonCevapla.Location = new System.Drawing.Point(193, 483);
+            this.buttonCevapla.Name = "buttonCevapla";
+            this.buttonCevapla.Size = new System.Drawing.Size(90, 42);
+            this.buttonCevapla.TabIndex = 26;
+            this.buttonCevapla.Text = "Başla";
+            this.buttonCevapla.UseVisualStyleBackColor = true;
+            this.buttonCevapla.Click += new System.EventHandler(this.buttonCevapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 542);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Doğru :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 542);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Yanlış :";
+            // 
+            // labelDogru
+            // 
+            this.labelDogru.AutoSize = true;
+            this.labelDogru.Location = new System.Drawing.Point(213, 542);
+            this.labelDogru.Name = "labelDogru";
+            this.labelDogru.Size = new System.Drawing.Size(18, 20);
+            this.labelDogru.TabIndex = 29;
+            this.labelDogru.Text = "0";
+            // 
+            // labelYanlis
+            // 
+            this.labelYanlis.AutoSize = true;
+            this.labelYanlis.Location = new System.Drawing.Point(313, 542);
+            this.labelYanlis.Name = "labelYanlis";
+            this.labelYanlis.Size = new System.Drawing.Size(18, 20);
+            this.labelYanlis.TabIndex = 30;
+            this.labelYanlis.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(470, 593);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.labelYanlis);
+            this.Controls.Add(this.labelDogru);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCevapla);
+            this.Controls.Add(this.buttonZ);
+            this.Controls.Add(this.buttonY);
+            this.Controls.Add(this.buttonV);
+            this.Controls.Add(this.buttonU);
+            this.Controls.Add(this.buttonT);
+            this.Controls.Add(this.buttonS);
+            this.Controls.Add(this.buttonR);
+            this.Controls.Add(this.buttonP);
+            this.Controls.Add(this.buttonO);
+            this.Controls.Add(this.buttonN);
+            this.Controls.Add(this.buttonM);
+            this.Controls.Add(this.buttonL);
+            this.Controls.Add(this.buttonK);
+            this.Controls.Add(this.buttonJ);
+            this.Controls.Add(this.buttonI);
+            this.Controls.Add(this.buttonH);
+            this.Controls.Add(this.buttonG);
+            this.Controls.Add(this.buttonF);
+            this.Controls.Add(this.buttonE);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonA);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Passaparola";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Button buttonE;
+        private System.Windows.Forms.Button buttonF;
+        private System.Windows.Forms.Button buttonG;
+        private System.Windows.Forms.Button buttonH;
+        private System.Windows.Forms.Button buttonI;
+        private System.Windows.Forms.Button buttonJ;
+        private System.Windows.Forms.Button buttonK;
+        private System.Windows.Forms.Button buttonL;
+        private System.Windows.Forms.Button buttonM;
+        private System.Windows.Forms.Button buttonN;
+        private System.Windows.Forms.Button buttonO;
+        private System.Windows.Forms.Button buttonP;
+        private System.Windows.Forms.Button buttonR;
+        private System.Windows.Forms.Button buttonS;
+        private System.Windows.Forms.Button buttonT;
+        private System.Windows.Forms.Button buttonU;
+        private System.Windows.Forms.Button buttonV;
+        private System.Windows.Forms.Button buttonY;
+        private System.Windows.Forms.Button buttonZ;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCevapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelDogru;
+        private System.Windows.Forms.Label labelYanlis;
+    }
+}
+
---
+++ Passaparola/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Passaparola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int iSoruNo = 1, iDogru = 0, iYanlis = 0, iSayac = 0;
+        string[] cevapAnahtari = { "", "Akdeniz", "Karadeniz" };
+        string[] sorular = { "", "Ülkemizin güneyindeki kıyı bölgesi neredir ?", "Ülkemizin en çok ormanlık bölgesi hangisidir ?" };
+
+        private void buttonSonraki_Click(object sender, EventArgs e)
+        {
+            iSoruNo++;
+            soru(iSoruNo);
+        }
+
+        public void cevap(int x)
+        {
+            switch (x)
+            {
+                case 1:
+                    if (textBox1.Text == cevapAnahtari[x])
+                    {
+                        buttonA.BackColor = Color.Green; iDogru++;
+                    }
+                    else
+                    {
+                        buttonA.BackColor = Color.Red; iYanlis++;
+                    }
+                    break;
+                case 2:
+                    if (textBox1.Text == cevapAnahtari[x])
+                    {
+                        buttonB.BackColor = Color.Green; iDogru++;
+                    }
+                    else
+                    {
+                        buttonB.BackColor = Color.Red; iYanlis++;
+                    }
+                    break;
+            }
+
+        }
+
+        public void soru(int x)
+        {
+            switch (x)
+            {
+                case 1:
+                    richTextBox1.Text = sorular[x];
+                    buttonA.BackColor = Color.Yellow;
+                    break;
+                case 2:
+                    richTextBox1.Text = sorular[x];
+                    buttonB.BackColor = Color.Yellow;
+                    break;
+            }
+        }
+
+        private void buttonCevapla_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" || buttonCevapla.Text == "Sonraki")
+            {
+                iSayac++;
+                if (textBox1.Text != "")
+                {
+                    cevap(iSoruNo);
+                    textBox1.Text = "";
+                    buttonCevapla.Text = "Sonraki";
+                }
+
+                if (iSayac == (3 * iSoruNo))
+                {
+                    buttonCevapla.Text = "Cevapla";
+                    iSoruNo++;
+                    soru(iSoruNo);
+                }
+
+                labelDogru.Text = iDogru.ToString();
+                labelYanlis.Text = iYanlis.ToString();
+            }
+            else if (textBox1.ReadOnly == false && textBox1.Text == "" && buttonCevapla.Text != "Sonraki")
+            {
+                MessageBox.Show("Lütfen bir yanıt giriniz.");
+            }
+
+            if (buttonCevapla.Text == "Başla")
+            {
+                textBox1.ReadOnly = false;
+                buttonCevapla.Text = "Cevapla";
+                soru(iSoruNo);
+                iSayac++;
+            }
+        }
+    }
+}
